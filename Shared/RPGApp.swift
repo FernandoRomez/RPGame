65dfaf37d448018f72d9af96dbb12754ec812ee3
Fernando Meza <fernandomezarod@gmail.com>
2022-04-19 23:34:52 -0500

    Initial Commit

---
+++ Shared/RPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPGApp.swift
+//  Shared
+//
+//  Created by fernando meza on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
